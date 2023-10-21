2eaf5655763afd44c39acd8dd2c2670db80113be
T3zis <quoqy@mail.ru>
2023-10-21 15:43:20 +0300

    Изменение в рендоме

--- 1/kr1.2/random2.c
+++ 1/kr1.2/random2.c
@@ -3,45 +3,44 @@
 #include <locale.h>
 #include <math.h>
 #include <time.h>
+/*
+*Ищем остаток от деления на 1000000 чтобы уменьшить числою
+*Умножаем на 0.1 чтобы число стало вещественным.
+*/
+#define RANDOM_DELIM  (rand()%100000)*0.1
 
 void random2(void)
 {
-  int  x_random = 0, y_random = 0;
-  double b = 0.0,  x_random1 = 0.0, y_random1 = 0.0;
+  double b = 0.0,  x_random = 0.0, y_random = 0.0;
 
   srand(time(NULL));
   
-  x_random = rand()%100000;
+  y_random = RANDOM_DELIM;
 
-  y_random = rand()%100000;
+  x_random = RANDOM_DELIM;
 
- 
-  x_random1 = x_random * 0.1;
-
-  y_random1 = y_random * 0.1;
-
-  printf("x = %f", x_random1);
+  printf("x = %f", x_random);
   puts("\n");
   
-  printf("y = %f", y_random1);
+  printf("y = %f", y_random);
   puts("\n");
 
-  if ((x_random1 / y_random1) > 0){
-    b = log(x_random1 / y_random1) + pow((x_random1 * x_random1 + y_random1), 3);
+  if ((x_random / y_random) > 0){
+    b = log(x_random / y_random) + pow((x_random * x_random + y_random), 3);
     puts("Ответ:");
     printf("%f", b);
     puts("\n");
   }
   
-  if ((x_random1 / y_random1) < 0){
-    b = log(fabs(x_random1 / y_random1)) + pow((x_random1 * x_random1 + y_random1), 3);
+  if ((x_random / y_random) < 0){
+    b = log(fabs(x_random / y_random)) + pow((x_random * x_random + y_random), 3);
     puts("Ответ:");        
     printf("%f", b);
     puts("\n");
   }
   
-  if (x_random1 == 0 && y_random1 != 0){
-    b = pow((x_random1 * x_random1 + y_random1), 3);
+  if (x_random == 0 && y_random != 0){
+    b = pow((x_random * x_random + y_random), 3);
     puts("Ответ:");
     printf("%f", b);
     puts("\n");
